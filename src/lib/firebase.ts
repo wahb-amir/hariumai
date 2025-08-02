f07b82831f2a297cba4419e1d3fdd7d0d4d7d390
Abdullah Chohan <abdull4hchohan@gmail.com>
2025-08-02 11:50:36 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,12 +7,13 @@ let auth: Auth;
 
 if (typeof window !== 'undefined') {
     const firebaseConfig = {
-      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || '',
-      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || '',
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || '',
-      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || '',
-      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '',
-      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || ''
+      apiKey: "AIzaSyC5qgRXT3sFCYqp23VhByLC-CjmlqsxkOY",
+      authDomain: "new-prototype-4j15w.firebaseapp.com",
+      databaseURL: "https://new-prototype-4j15w-default-rtdb.firebaseio.com",
+      projectId: "new-prototype-4j15w",
+      storageBucket: "new-prototype-4j15w.firebasestorage.app",
+      messagingSenderId: "1048797164176",
+      appId: "1:1048797164176:web:ef96f32a3a905351cc1e15"
     };
     app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
     auth = getAuth(app);
